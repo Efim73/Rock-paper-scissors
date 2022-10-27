ba3fb0ba555a34d18348a6bf823e2adaa0e13b0d
Efim73 <efimtop789@gmail.com>
2022-10-27 10:38:06 +0300

    hello

--- rps/src/App.js
+++ rps/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloadeeee
+          Edit <code>src/App.js</code> and save to relode!!
         </p>
         <a
           className="App-link"
