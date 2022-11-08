99955fd726a283045960bbe9fb608e628b28d5d5
Efim73 <efimtop789@gmail.com>
2022-11-08 10:30:34 +0300

    start

--- rps/src/App.js
+++ rps/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to relode!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div id='game'>
+        <div id='gradient'></div>
+        <h1>ROCK PAPER SCISSORS</h1>
+        <h2>3</h2>
+        <div id="hands">
+          <img src="images/paper.svg" alt="" />
+          <img src="images/rock.svg" alt="" />
+        </div>
+        <div id="options">
+          <img src="images/rock.svg" alt="" />
+          <img src="images/paper.svg" alt="" />
+          <img src="images/scissors.svg" alt="" />
+        </div>
+      </div>
+    )
+  }
+
+
 }
+
+
 
 export default App;
