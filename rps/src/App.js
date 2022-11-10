b57158871fcdc01aee37c50d3ca45402e8d27ecd
Efim73 <efimtop789@gmail.com>
2022-11-10 11:53:16 +0300

    counter added

--- rps/src/App.js
+++ rps/src/App.js
@@ -7,23 +7,50 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      images:['rock.svg', 'paper.svg', 'scissors.svg'],
+      counter: 3,
+      
     }
+  }
+  optionsButton(e, id){
+    if(this.state.counter==0){
+      this.setState({
+        counter:3
+      })
+    }
+    for(let i=0; i<3; i++){
+      setTimeout(()=>{
+        // console.log(this.state.counter);
+        this.setState(function (state){
+          return{
+    
+            counter: state.counter-1,
+          }
+        })
+      },1000+i*1000)
+    }
+
+    console.log(id);
   }
   render() {
     return (
       <div id='game'>
         <div id='gradient'></div>
         <h1>ROCK PAPER SCISSORS</h1>
-        <h2>3</h2>
+        <h2>{this.state.counter}</h2>
         <div id="hands">
           <img src="images/paper.svg" alt="" />
           <img src="images/rock.svg" alt="" />
         </div>
         <div id="options">
-          <img src="images/rock.svg" alt="" />
-          <img src="images/paper.svg" alt="" />
-          <img src="images/scissors.svg" alt="" />
+          {
+
+              this.state.images.map((image, id)=>(
+                
+               <img src={"images/"+image} alt="" onClick={(e) => this.optionsButton(e, id)} key={id}/>
+              ))
+          }
+
         </div>
       </div>
     )
@@ -35,3 +62,6 @@ class App extends React.Component {
 
 
 export default App;
+
+
+// менять левую руку на выбранный символ и правую на рандом
