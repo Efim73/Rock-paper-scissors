45704d67432717d6e75bcb620a1134a72a30acbf
Efim73 <efimtop789@gmail.com>
2022-11-15 12:00:42 +0300

    effects

--- rps/src/App.js
+++ rps/src/App.js
@@ -9,24 +9,35 @@ class App extends React.Component {
     this.state = {
       images:['rock.svg', 'paper.svg', 'scissors.svg'],
       counter: 3,
-      
+      bot: 0,
+      playerHand: 0,
+      counterClass: '',  
+      buttonClass: '',
+      activeButton: null,
     }
   }
   optionsButton(e, id){
-    if(this.state.counter==0){
-      this.setState({
-        counter:3
-      })
-    }
-    for(let i=0; i<3; i++){
+    this.setState({
+      counterClass: 'counterVisible',
+      buttonClass: 'buttonBlocked',
+      activeButton: id,
+    })
+    for(let i=0; i<4; i++){
       setTimeout(()=>{
         // console.log(this.state.counter);
         this.setState(function (state){
           return{
-    
+            bot : Math.floor(Math.random()*3),  
             counter: state.counter-1,
           }
         })
+        if(i==3){
+          this.setState({
+            counter: 3,
+            counterClass: '',  
+            buttonClass: '',
+          })
+        }
       },1000+i*1000)
     }
 
@@ -37,17 +48,17 @@ class App extends React.Component {
       <div id='game'>
         <div id='gradient'></div>
         <h1>ROCK PAPER SCISSORS</h1>
-        <h2>{this.state.counter}</h2>
+        <h2 className={this.state.counterClass}>{this.state.counter}</h2>
         <div id="hands">
-          <img src="images/paper.svg" alt="" />
-          <img src="images/rock.svg" alt="" />
+          <img src={"images/"+this.state.images[this.state.playerHand]} alt="" />
+          <img src={"images/"+this.state.images[this.state.bot]} alt="" />
         </div>
         <div id="options">
           {
 
               this.state.images.map((image, id)=>(
                 
-               <img src={"images/"+image} alt="" onClick={(e) => this.optionsButton(e, id)} key={id}/>
+               <img className={this.state.buttonClass + (this.state.activeButton==id? ' activeButton' : '')} src={"images/"+image} alt="" onClick={(e) => this.optionsButton(e, id)} key={id}/>
               ))
           }
 
@@ -64,4 +75,4 @@ class App extends React.Component {
 export default App;
 
 
-// менять левую руку на выбранный символ и правую на рандом
+// менять левую руку на выбранный символ и правую на рандом Доделать
